bf50b63a60f3af830da25393d5804a171579caa2
Joaquin Candalaft <joaquin.candalaft@gmail.com>
2024-05-13 18:28:17 -0300

    first commit with dev-env on concurrently and halfway docker deploy

---
+++ backend/src/app.ts
@@ -0,0 +1,50 @@
+import express from "express";
+import cors from "cors";
+
+import {
+  connectToDb,
+  getDatabaseClient,
+  getIsDatabaseConnected,
+} from "./databaseConnection";
+
+connectToDb();
+
+const mongodbClient = getDatabaseClient();
+
+const app = express();
+const port = 8081;
+
+const MAZMO_API_URL = "https://prod.mazmoapi.net";
+
+const corsOptions = {
+  origin: [
+    MAZMO_API_URL,
+    "joka.vip",
+    "https://joka.vip",
+    "http://localhost:3000",
+  ],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  console.log("Whatever");
+  res.send("Hello World!");
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.get("/mongodb-health", async (req, res) => {
+  if (await getIsDatabaseConnected()) {
+    res.send("OK");
+  } else {
+    res.status(502);
+    res.send("ERROR");
+  }
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
---
+++ backend/src/databaseConnection.ts
@@ -0,0 +1,31 @@
+import { MongoClient } from "mongodb";
+
+export const CONNECTION_STRING = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:27017/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(CONNECTION_STRING);
+
+export const connectToDb = async () => {
+  console.log(CONNECTION_STRING);
+  try {
+    await client.connect();
+    console.log("Connected to the database");
+    return client;
+  } catch (error) {
+    console.error("Error connecting to the database", error);
+    return null;
+  }
+};
+
+export const getIsDatabaseConnected = async () => {
+  try {
+    const isConnected = await client.db("admin").command({ ping: 1 });
+    return true;
+  } catch (error) {
+    console.error("Error pinging the database", error);
+    return false;
+  }
+};
+
+export const getDatabaseClient = () => {
+  return client;
+};

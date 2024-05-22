8f2cf9034a6c27532a9b6e7a418fc27aec4c0322
Joaquin Candalaft <joaquin.candalaft@gmail.com>
2024-05-22 13:03:36 -0300

    fix db connection

--- backend/src/databaseConnection.ts
+++ backend/src/databaseConnection.ts
@@ -1,11 +1,13 @@
 import { MongoClient } from "mongodb";
 
-export const CONNECTION_STRING = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// export const CONNECTION_STRING = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
+export const CONNECTION_STRING = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority&appName=casino-cluster-dev`;
+
+// console.log(CONNECTION_STRING);
 const client = new MongoClient(CONNECTION_STRING);
 
 export const connectToDb = async () => {
-  console.log(CONNECTION_STRING);
   try {
     await client.connect();
     console.log("Connected to the database");

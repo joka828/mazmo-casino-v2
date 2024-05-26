8048aee9dc117a17f2a049297d08fe927f8caa15
Joaquin Candalaft <joaquin.candalaft@gmail.com>
2024-05-26 16:47:39 -0300

    add mazmo to cors to request from iframe

---
+++ backend/src/roulette/types.ts
@@ -0,0 +1,60 @@
+export interface RouletteUser {
+  id: string;
+  name: string;
+  color: string;
+}
+
+export type BetPlace =
+  | "red"
+  | "black"
+  | "even"
+  | "odd"
+  | "firstHalf"
+  | "secondHalf"
+  | "firstDozen"
+  | "secondDozen"
+  | "thirdDozen"
+  | "0"
+  | "1"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "10"
+  | "11"
+  | "12"
+  | "13"
+  | "14"
+  | "15"
+  | "16"
+  | "17"
+  | "18"
+  | "19"
+  | "20"
+  | "21"
+  | "22"
+  | "23"
+  | "24"
+  | "25"
+  | "26"
+  | "27"
+  | "28"
+  | "29"
+  | "30"
+  | "31"
+  | "32"
+  | "33"
+  | "34"
+  | "35"
+  | "36";
+
+export interface RouletteState {
+  spinId?: string;
+  users: Record<string, RouletteUser>;
+  bets: Record<BetPlace, Record<RouletteUser["id"], number>>;
+  status: "inactive" | "openBets" | "spinning" | "finished";
+}

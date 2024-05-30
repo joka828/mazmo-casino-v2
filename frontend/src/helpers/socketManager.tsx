8a1b16d436ecbf37b64846992dc42bf82ca43f56
Joaquin Candalaft <joaquin.candalaft@gmail.com>
2024-05-30 11:28:35 -0300

    fix socket bug receiving multiple times the same event

--- frontend/src/helpers/socketManager.tsx
+++ frontend/src/helpers/socketManager.tsx
@@ -24,39 +24,40 @@ export default function useSocketEvents() {
 
     socket.on("connect", () => {
       setSocketStatus(true);
-      socket.on("initialize", (initialData: { roulette: RouletteRound }) => {
-        console.log("initialize", initialData);
-        if (initialData.roulette.id) {
-          rouletteState.initializeData(initialData.roulette);
-        } else {
-          rouletteState.setRouletteStatus(initialData.roulette.status);
-        }
-        rouletteState.setRouletteStatus(initialData.roulette.status);
-      });
-
-      socket.on(
-        ROULETTE_BET_ADDED,
-        (data: { betPlace: BetPlace; amount: number; user: RouletteUser }) => {
-          rouletteState.addBet(data.betPlace, data.amount, data.user);
-          console.log("bet added", data);
-        }
-      );
-
-      socket.on(
-        ROULETTE_ROUND_CREATED,
-        (data: { roundId: string; finishTimestamp: number }) => {
-          rouletteState.startRound(data.roundId, data.finishTimestamp);
-          console.log("round created", data);
-        }
-      );
-
-      socket.on(
-        ROULETTE_ROUND_ENDED,
-        (data: { winnerNumber: number; winners: RouletteState["winners"] }) => {
-          rouletteState.noMoreBets(data.winnerNumber, data.winners);
-          console.log("round ended, no more bets", data);
-        }
-      );
     });
+
+    socket.on("initialize", (initialData: { roulette: RouletteRound }) => {
+      console.log("initialize", initialData);
+      if (initialData.roulette.id) {
+        rouletteState.initializeData(initialData.roulette);
+      } else {
+        rouletteState.setRouletteStatus(initialData.roulette.status);
+      }
+      rouletteState.setRouletteStatus(initialData.roulette.status);
+    });
+
+    socket.on(
+      ROULETTE_BET_ADDED,
+      (data: { betPlace: BetPlace; amount: number; user: RouletteUser }) => {
+        rouletteState.addBet(data.betPlace, data.amount, data.user);
+        console.log("bet added", data);
+      }
+    );
+
+    socket.on(
+      ROULETTE_ROUND_CREATED,
+      (data: { roundId: string; finishTimestamp: number }) => {
+        rouletteState.startRound(data.roundId, data.finishTimestamp);
+        console.log("round created", data);
+      }
+    );
+
+    socket.on(
+      ROULETTE_ROUND_ENDED,
+      (data: { winnerNumber: number; winners: RouletteState["winners"] }) => {
+        rouletteState.noMoreBets(data.winnerNumber, data.winners);
+        console.log("round ended, no more bets", data);
+      }
+    );
   }, []);
 }

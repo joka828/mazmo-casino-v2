9200db8f6b7e2d2ebecf5c96092d1d1a4ef270f1
Joaquin Candalaft <joaquin.candalaft@gmail.com>
2024-06-02 22:30:29 -0300

    fix db connection

--- backend/src/helpers/dbManager.ts
+++ backend/src/helpers/dbManager.ts
@@ -1,10 +1,15 @@
 import { MongoClient } from "mongodb";
 
-export const CONNECTION_STRING = `mongodb${
-  process.env.DB_HOST === "localhost" ? "" : "+srv"
-}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${
-  process.env.DB_NAME
-}?authSource=${process.env.DB_NAME}&retryWrites=true&w=majority`;
+// export const CONNECTION_STRING = `mongodb${
+//   process.env.DB_HOST === "localhost" ? "" : "+srv"
+// }://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${
+//   process.env.DB_NAME
+// }?authSource=${process.env.DB_NAME}&retryWrites=true&w=majority`;
+
+export const CONNECTION_STRING =
+  process.env.DB_HOST === "localhost"
+    ? `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?authSource=${process.env.DB_NAME}&retryWrites=true&w=majority`
+    : `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(CONNECTION_STRING);
 
